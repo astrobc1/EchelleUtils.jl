acb1c935316acb58e70c3caae0307a4de3975a80
Bryson Cale <bryson.cale1@gmail.com>
2022-10-25 14:33:40 -0700

    updates

--- src/Maths.jl
+++ src/Maths.jl
@@ -32,6 +32,22 @@ function round_half_down(x)
     return ceil(x - 0.5)
 end
 
+function group_peaks(x; sep)
+    peak_centers = Float64[]
+    peak_heights = Float64[]
+    prev_i = 1
+    for i=1:length(x) - 1
+        if x[i+1] - x[i] > sep
+            push!(peak_centers, (x[prev_i] + x[i]) / 2)
+            push!(peak_heights, x[i] - x[prev_i])
+            prev_i = i + 1
+        end
+    end
+    push!(peak_centers, (x[prev_i] + x[end]) / 2)
+    push!(peak_heights, x[end] - x[prev_i])
+    return peak_centers, peak_heights
+end
+
 include("chebyshev.jl")
 include("stats.jl")
 include("filters.jl")
