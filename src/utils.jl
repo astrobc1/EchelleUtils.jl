323d94a6231a834ed48411c8dcb2953c6ce87e48
Bryson Cale <bryson.cale1@gmail.com>
2022-11-30 10:24:11 -0800

    new util macros for debugging

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,25 @@
 export flatten_jagged_vector, get_inds1d
 
+macro tryignore(expr)
+    quote
+        try
+            esc(expr)
+        catch e
+            @error "Error reached but ignored with @tryignore!" exception=(e, catch_backtrace())
+        end
+    end
+end
+
+macro debugerror(expr)
+    quote
+        try
+            esc(expr)
+        catch
+            @infiltrate
+        end
+    end
+end
+
 const COLORS_HEX_GADFLY = [
     "#00BEFF", "#D4CA3A", "#FF6DAE", "#67E1B5", "#EBACFA",
     "#9E9E9E", "#F1988E", "#5DB15A", "#E28544", "#52B8AA"
